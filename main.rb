1b75dc05863a77423486335c19916ad2186951ca
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-01-10 18:15:23 +0530

    chore: update main.rb

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require 'debug'
 require_relative 'lib/binary_search_tree'
+# binding.break
 
 arr = [7, 7, 6, 5, 4, 3, 2, 1, 1]
 
@@ -14,5 +15,25 @@ bst.insert(8)
 
 bst.insert(9)
 bst.insert(10)
+
+bst.to_s
+
+bst.delete(4)
+
+bst.to_s
+
+bst.delete(7)
+
+bst.to_s
+
+bst.delete(7)
+
+bst.to_s
+
+bst.delete(7)
+
+bst.to_s
+
+bst.delete(10)
 
 bst.to_s
